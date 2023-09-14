66151c7ccc996896aea59f9615deeb8f5bd69bc2
HP <HP@DESKTOP-CHRD5CU>
2023-09-14 10:42:40 +0530

    Add project files.

---
+++ E-Shop1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Shop1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
